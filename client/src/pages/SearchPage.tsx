eb6a46a72b899edaacbe3820c26ad1551e4241a7
rogergrubb <roger@grubb.net>
2025-11-24 11:52:53 -0800

    Fix: Add id and name attributes to search form inputs
    Added unique id and name attributes to:
    - Radius selector (search-radius)
    - Query search input (search-query)
    
    Improves form accessibility and fixes Chrome DevTools warnings.

--- client/src/pages/SearchPage.tsx
+++ client/src/pages/SearchPage.tsx
@@ -163,8 +163,12 @@ export default function SearchPage() {
             
             {/* Radius Selector */}
             <div className="flex items-center gap-2">
-              <span className="text-sm text-gray-600">Within</span>
+              <label htmlFor="search-radius" className="text-sm text-gray-600">
+                Within
+              </label>
               <select
+                id="search-radius"
+                name="searchRadius"
                 value={radius}
                 onChange={(e) => setRadius(parseInt(e.target.value))}
                 className="px-3 py-2 border rounded-lg focus:ring-2 focus:ring-blue-500"
@@ -194,6 +198,8 @@ export default function SearchPage() {
             <div className="flex-1 relative">
               <Search className="absolute left-3 top-1/2 -translate-y-1/2 w-5 h-5 text-gray-400" />
               <input
+                id="search-query"
+                name="searchQuery"
                 type="text"
                 value={query}
                 onChange={(e) => setQuery(e.target.value)}
@@ -331,4 +337,3 @@ export default function SearchPage() {
     </div>
   );
 }
-
