6630f5517ef87149d1975cea3f4ebc9d814e60a6
rogergrubb <roger@grubb.net>
2026-01-30 23:22:14 -0800

    fix: Correct sitemap.ts string literals (build fix)

--- server/routes/sitemap.ts
+++ server/routes/sitemap.ts
@@ -51,37 +51,25 @@ router.get("/sitemap.xml", async (req, res) => {
     }));
 
     // Build XML
-    let xml = '<?xml version="1.0" encoding="UTF-8"?>
-';
-    xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-';
+    let xml = '<?xml version="1.0" encoding="UTF-8"?>\n';
+    xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
 
     // Add static pages
     staticPages.forEach(page => {
-      xml += "  <url>
-";
-      xml += `    <loc>${baseUrl}${page.url}</loc>
-`;
-      xml += `    <changefreq>${page.changefreq}</changefreq>
-`;
-      xml += `    <priority>${page.priority}</priority>
-`;
-      xml += "  </url>
-";
+      xml += "  <url>\n";
+      xml += `    <loc>${baseUrl}${page.url}</loc>\n`;
+      xml += `    <changefreq>${page.changefreq}</changefreq>\n`;
+      xml += `    <priority>${page.priority}</priority>\n`;
+      xml += "  </url>\n";
     });
 
     // Add category pages
     categoryPages.forEach(page => {
-      xml += "  <url>
-";
-      xml += `    <loc>${baseUrl}${page.url}</loc>
-`;
-      xml += `    <changefreq>${page.changefreq}</changefreq>
-`;
-      xml += `    <priority>${page.priority}</priority>
-`;
-      xml += "  </url>
-";
+      xml += "  <url>\n";
+      xml += `    <loc>${baseUrl}${page.url}</loc>\n`;
+      xml += `    <changefreq>${page.changefreq}</changefreq>\n`;
+      xml += `    <priority>${page.priority}</priority>\n`;
+      xml += "  </url>\n";
     });
 
     // Add listing pages
@@ -90,18 +78,12 @@ router.get("/sitemap.xml", async (req, res) => {
         ? new Date(listing.updatedAt).toISOString().split('T')[0]
         : new Date().toISOString().split('T')[0];
       
-      xml += "  <url>
-";
-      xml += `    <loc>${baseUrl}/listings/${listing.id}</loc>
-`;
-      xml += `    <lastmod>${lastmod}</lastmod>
-`;
-      xml += `    <changefreq>weekly</changefreq>
-`;
-      xml += `    <priority>0.6</priority>
-`;
-      xml += "  </url>
-";
+      xml += "  <url>\n";
+      xml += `    <loc>${baseUrl}/listings/${listing.id}</loc>\n`;
+      xml += `    <lastmod>${lastmod}</lastmod>\n`;
+      xml += `    <changefreq>weekly</changefreq>\n`;
+      xml += `    <priority>0.6</priority>\n`;
+      xml += "  </url>\n";
     });
 
     xml += "</urlset>";
@@ -115,4 +97,3 @@ router.get("/sitemap.xml", async (req, res) => {
 });
 
 export default router;
-
